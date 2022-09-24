5a2f1cbba3586337ecb607ef6c3f88e0e00f7b07
Pierre-Monier <p.monier96@gmail.com>
2022-09-24 17:55:37 +0200

    feat: add social media link

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_canvart/noise_orbit/controller/noise_orbit_controller_provider.dart';
 import 'package:flutter_canvart/noise_orbit/noise_orbit.dart';
 import 'package:flutter_canvart/noise_orbit/tool/noise_orbit_configuration.dart';
+import 'package:flutter_canvart/shared/style/app_icons.dart';
 import 'package:flutter_canvart/shared/widget/configuration_modal.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,26 +33,49 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
 
+  void openLink(String path) {
+    final uri = Uri.parse(path);
+    launchUrl(uri, mode: LaunchMode.externalApplication);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text("Perlin Noise Orbit"),
+        actions: [
+          IconButton(
+            onPressed: () {
+              openLink("https://twitter.com/pierremonier9");
+            },
+            icon: const Icon(AppIcons.twitter),
+          ),
+          IconButton(
+            onPressed: () {
+              openLink("https://github.com/Pierre-Monier/flutter_cool_visuals");
+            },
+            icon: const Icon(AppIcons.github),
+          ),
+        ],
+      ),
       body: const SizedBox.expand(
         child: NoiseOrbit(),
       ),
       floatingActionButton: FloatingActionButton.extended(
-        label: const Text("Edit"),
-        icon: const Icon(Icons.edit),
         onPressed: () {
           showModalBottomSheet(
             context: context,
             barrierColor: Colors.transparent,
             backgroundColor: Colors.transparent,
             builder: (context) {
-              return const ConfigurationModal(child: NoiseOrbitConfiguration());
+              return const ConfigurationModal(
+                child: NoiseOrbitConfiguration(),
+              );
             },
           );
         },
-        // child: Icon(Icons.settings),
+        icon: const Icon(Icons.edit),
+        label: const Text("Edit"),
       ),
     );
   }
---
+++ lib/shared/style/app_icons.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+@immutable
+class AppIcons {
+  const AppIcons._();
+
+  static const fontFamily = 'Remix Icon';
+
+  static const github = IconData(0xEDCA, fontFamily: fontFamily);
+  static const twitter = IconData(0xF23A, fontFamily: fontFamily);
+}
